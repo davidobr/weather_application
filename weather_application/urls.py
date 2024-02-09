856ddc6eb635f90c5e74805890d9f96415e2c399
DavidOBr <david.obrien.nord@gmail.com>
2024-02-09 22:21:26 +0000

    new weather_application

--- weather_application/urls.py
+++ weather_application/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for weather_app project.
+URL configuration for weather_application project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from website.views import homepage
+from weather_application.views import homepage
 
 urlpatterns = [
     path('', homepage, name="home"),
