3179376993220fff54831476cc003a7368e858cf
DavidOBr <david.obrien.nord@gmail.com>
2024-02-09 22:14:30 +0000

    Revert "initial commit"
    This reverts commit 2f7813b797ebbe7c2414fbec949cd757f9ff0b35.

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for weather_application project.
+URL configuration for weather_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from weather_application.views import homepage
+from website.views import homepage
 
 urlpatterns = [
     path('', homepage, name="home"),
